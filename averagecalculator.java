f1eafb117d0a71a57d61468c12cb0f7ddb805584
richadalal <65190493+richadalal@users.noreply.github.com>
2021-01-05 20:43:43 -0500

    Update averagecalculator.java

--- averagecalculator.java
+++ averagecalculator.java
@@ -1,47 +1,41 @@
-import hsa.Console;
-import java.awt.*;
-import javax.imageio.*;
-import java.io.*;
+//Richa Dalal
+//Average Calculator 
+
+import java.util.Scanner;
 
 public class averagecalculator
-{ static Console c;
+{ 
+public static void main(String[] args)   
 
-  public static void main(String[] args)
-      {  c = new Console(50, 100);
-    //Set the console
-
-    String name;
-   
-    Double markbiology, markmath, markenglish, markfrench;
-
+  { 
+    Scanner sc= new Scanner(System.in);
+  
+    String name; 
+    Double markphysics, markmath, markcomputerscience, markfrench;
     Double average;    
            
-    //Enter in name
-          
-          c.println("Please enter your name:");
-    name = c.readLine(); 
+    System.out.println("Please enter your name:");
+    name = sc.nextLine(); 
     
-    //Enter marks 
-     c.println("Enter your mark for physics:");
-         markphysics = c.readDouble();
-     c.println("Enter your mark for math:");
-         markmath = c.readDouble();
-     c.println("Enter your mark for computerscience:");
-         markcomputerscience = c.readDouble();
-     c.println("Enter your mark for french:");
-         markfrench = c.readDouble();
+     System.out.println("Enter your mark for physics:");
+         markphysics = sc.nextDouble(); 
+     System.out.println("Enter your mark for math:");
+         markmath = sc.nextDouble(); 
+     System.out.println("Enter your mark for computerscience:");
+         markcomputerscience = sc.nextDouble(); 
+     System.out.println("Enter your mark for french:");
+         markfrench = sc.nextDouble(); 
          
      //Calculate the average of the marks 
-     average = ((markbiology + markmath + markenglish + markfrench)/4);
+     average = ((markphysics + markmath + markcomputerscience + markfrench)/4);
      
      //Output page 
-     c.println ("Your name is:" + name);
-     c.println ("Your physics mark is:" + markphysics);
-     c.println ("Your math mark is:" + markmath);
-     c.println ("Your computer science mark is:" + markcomputerscience);
-     c.println ("Your french mark is:" + markfrench);
-     c.println ("Your average is:" + average);
-     
+     System.out.println ("Your name is: " + name);
+     System.out.println ("Your physics mark is: " + markphysics);
+     System.out.println ("Your math mark is: " + markmath); 
+     System.out.println ("Your computer science mark is: " + markcomputerscience);
+     System.out.println ("Your french mark is: " + markfrench);
+     System.out.println ("Your average is:" + average);
      
     }
   }
